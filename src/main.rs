3d925bb9568257c9aa5d1ba2b05581be9a0a350b
Wuttel <matthias.vanrompuy@hotmail.com>
2026-05-07 18:00:32 +0200

    feat: auth.log parser geimplementeerd en getest

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ mod storage;
 mod output;
 
 use models::event::{Event, IntentResult};
+use std::fs::read_to_string;
+use parser::auth::parse;
 
 fn main() {
+
     println!("intentiq - cybersecurity log analyzer");
+    let contents = read_to_string("logs/auth.log").expect("Could not read file");
+    let parsed_content = parse(&contents);
+    println!("{:#?}", parsed_content);
+
+    
 }
